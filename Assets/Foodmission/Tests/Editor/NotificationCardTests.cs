3b1a5dc38ff988865c009daeb31592fc92282d30
Antonio Jose Duran Sanchez <tonidurans@gmail.com>
2026-03-31 10:17:20 +0200

    Added NotificationCard component with Bind, OnView, OnDelete

---
+++ Assets/Foodmission/Tests/Editor/NotificationCardTests.cs
@@ -0,0 +1,102 @@
+using NUnit.Framework;
+using eu.foodmission.platform;
+using UnityEngine.UIElements;
+using Unity.AppUI.UI;
+
+namespace eu.foodmission.platform.Tests
+{
+    [TestFixture]
+    public class NotificationCardTests
+    {
+        private VisualTreeAsset _template;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _template = UnityEditor.AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(
+                "Assets/Foodmission/AppUI/Template/NotificationCard.uxml");
+        }
+
+        [Test]
+        public void Bind_SetsTextLabel()
+        {
+            var card = new NotificationCard(_template);
+            var model = new NotificationModel
+            {
+                Id = "1", Text = "Hello world", Timestamp = "1 h",
+                Type = NotificationType.Social, IsRead = false
+            };
+
+            card.Bind(model);
+
+            var label = card.Q<Label>("text");
+            Assert.AreEqual("Hello world", label.text);
+        }
+
+        [Test]
+        public void Bind_SetsTimestampLabel()
+        {
+            var card = new NotificationCard(_template);
+            var model = new NotificationModel
+            {
+                Id = "2", Text = "Test", Timestamp = "2 h",
+                Type = NotificationType.Badge, IsRead = false
+            };
+
+            card.Bind(model);
+
+            var label = card.Q<Label>("timestamp");
+            Assert.AreEqual("2 h", label.text);
+        }
+
+        [Test]
+        public void Bind_UnreadNotification_DoesNotHaveReadClass()
+        {
+            var card = new NotificationCard(_template);
+            var model = new NotificationModel
+            {
+                Id = "3", Text = "Test", Timestamp = "1 h",
+                Type = NotificationType.System, IsRead = false
+            };
+
+            card.Bind(model);
+
+            Assert.IsFalse(card.ClassListContains("fm-notification-card--read"));
+        }
+
+        [Test]
+        public void Bind_ReadNotification_HasReadClass()
+        {
+            var card = new NotificationCard(_template);
+            var model = new NotificationModel
+            {
+                Id = "4", Text = "Test", Timestamp = "1 h",
+                Type = NotificationType.System, IsRead = true
+            };
+
+            card.Bind(model);
+
+            Assert.IsTrue(card.ClassListContains("fm-notification-card--read"));
+        }
+
+        [Test]
+        public void Bind_CalledTwice_UpdatesState()
+        {
+            var card = new NotificationCard(_template);
+            var model = new NotificationModel
+            {
+                Id = "5", Text = "First", Timestamp = "1 h",
+                Type = NotificationType.Social, IsRead = false
+            };
+            card.Bind(model);
+
+            model.Text = "Second";
+            model.IsRead = true;
+            card.Bind(model);
+
+            var label = card.Q<Label>("text");
+            Assert.AreEqual("Second", label.text);
+            Assert.IsTrue(card.ClassListContains("fm-notification-card--read"));
+        }
+    }
+}
---
+++ Assets/Foodmission/scripts/AppUI/Components/NotificationCard.cs
@@ -0,0 +1,64 @@
+using System;
+
+using Unity.AppUI.UI;
+
+using UnityEngine.UIElements;
+
+namespace eu.foodmission.platform
+{
+    /// <summary>
+    /// Reusable notification card component.
+    /// Instantiated from NotificationCard.uxml and populated via Bind().
+    /// </summary>
+    public class NotificationCard : VisualElement
+    {
+        private Label _textLabel;
+        private Label _timestampLabel;
+        private ActionButton _overflowBtn;
+        private string _currentId;
+
+        public event Action<string> OnView;
+        public event Action<string> OnDelete;
+
+        public NotificationCard(VisualTreeAsset template)
+        {
+            template.CloneTree(this);
+            AddToClassList("fm-notification-card-wrapper");
+
+            _textLabel      = this.Q<Label>("text");
+            _timestampLabel = this.Q<Label>("timestamp");
+            _overflowBtn    = this.Q<ActionButton>("overflow-btn");
+
+            if (_overflowBtn != null)
+            {
+                _overflowBtn.clicked += OnOverflowClicked;
+            }
+        }
+
+        /// <summary>
+        /// Populates the card with data from the given model.
+        /// Can be called multiple times to rebind.
+        /// </summary>
+        public void Bind(NotificationModel model)
+        {
+            _currentId = model.Id;
+
+            if (_textLabel != null)
+            {
+                _textLabel.text = model.Text;
+            }
+
+            if (_timestampLabel != null)
+            {
+                _timestampLabel.text = model.Timestamp;
+            }
+
+            EnableInClassList("fm-notification-card--read", model.IsRead);
+        }
+
+        private void OnOverflowClicked()
+        {
+            OnView?.Invoke(_currentId);
+        }
+    }
+}
