cad03be3f5cd5fb185faa972c1bbb6e7eaef7d8b
Antonio Jose Duran Sanchez <tonidurans@gmail.com>
2026-04-22 10:24:26 +0200

    - Added FMDialog ShowAlert and ShowConfirm

---
+++ Assets/Foodmission/scripts/AppUI/Components/FMDialog.cs
@@ -0,0 +1,53 @@
+using System;
+
+using Unity.AppUI.UI;
+
+using UnityEngine.UIElements;
+
+namespace eu.foodmission.platform.Components
+{
+    public static class FMDialog
+    {
+        public static void ShowAlert(
+            VisualElement anchor,
+            string title,
+            string message,
+            AlertSemantic semantic = AlertSemantic.Information,
+            string okLabel = "@UI:TXT_OK",
+            Action onOk = null)
+        {
+            var dialog = new AlertDialog
+            {
+                title = title,
+                description = message,
+                variant = semantic
+            };
+
+            dialog.SetPrimaryAction(0, okLabel, () => onOk?.Invoke());
+
+            Modal.Build(anchor, dialog).Show();
+        }
+
+        public static void ShowConfirm(
+            VisualElement anchor,
+            string title,
+            string message,
+            Action onConfirm,
+            Action onCancel = null,
+            string confirmLabel = "@UI:TXT_ACCEPT",
+            string cancelLabel = "@UI:TXT_CANCEL")
+        {
+            var dialog = new AlertDialog
+            {
+                title = title,
+                description = message,
+                variant = AlertSemantic.Information
+            };
+
+            dialog.SetPrimaryAction(0, confirmLabel, () => onConfirm?.Invoke());
+            dialog.SetCancelAction(1, cancelLabel, () => onCancel?.Invoke());
+
+            Modal.Build(anchor, dialog).Show();
+        }
+    }
+}
