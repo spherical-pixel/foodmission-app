798283e260479224047f52d421c58385c9d8fcf2
Antonio Jose Duran Sanchez <tonidurans@gmail.com>
2026-04-22 10:28:17 +0200

    - Added FMDialog ShowScrollable and ShowCustom

--- Assets/Foodmission/scripts/AppUI/Components/FMDialog.cs
+++ Assets/Foodmission/scripts/AppUI/Components/FMDialog.cs
@@ -8,7 +8,7 @@ namespace eu.foodmission.platform.Components
 {
     public static class FMDialog
     {
-public static void ShowAlert(
+        public static void ShowAlert(
             VisualElement anchor,
             string title,
             string message,
@@ -28,7 +28,7 @@ public static void ShowAlert(
             Modal.Build(anchor, dialog).Show();
         }
 
-public static void ShowConfirm(
+        public static void ShowConfirm(
             VisualElement anchor,
             string title,
             string message,
@@ -47,6 +47,58 @@ public static void ShowConfirm(
 
             dialog.SetPrimaryAction(0, confirmLabel, onConfirm ?? (() => { }));
             dialog.SetCancelAction(1, cancelLabel, onCancel ?? (() => { }));
+
+            Modal.Build(anchor, dialog).Show();
+        }
+
+        public static void ShowScrollable(
+            VisualElement anchor,
+            string title,
+            string content,
+            Action onAccept = null,
+            Action onCancel = null,
+            string acceptLabel = "@UI:TXT_ACCEPT",
+            string cancelLabel = "@UI:TXT_BACK")
+        {
+            var dialog = new Dialog { title = title };
+
+            var scrollView = new ScrollView();
+            scrollView.AddToClassList("fm-dialog-scroll");
+
+            var text = new Text { text = content };
+            text.AddToClassList("fm-dialog-scroll-text");
+
+            scrollView.Add(text);
+            dialog.Add(scrollView);
+
+            dialog.SetPrimaryAction(0, acceptLabel, onAccept ?? (() => { }));
+            dialog.SetCancelAction(1, cancelLabel, onCancel ?? (() => { }));
+
+            Modal.Build(anchor, dialog).Show();
+        }
+
+        public static void ShowCustom(
+            VisualElement anchor,
+            string title,
+            VisualElement content,
+            params FMDialogAction[] actions)
+        {
+            var dialog = new Dialog { title = title };
+            dialog.Add(content);
+
+            int priority = 0;
+            foreach (var action in actions)
+            {
+                var captured = action;
+                if (captured.IsPrimary)
+                {
+                    dialog.SetPrimaryAction(priority++, captured.Label, captured.Callback ?? (() => { }));
+                }
+                else
+                {
+                    dialog.SetCancelAction(priority++, captured.Label, captured.Callback ?? (() => { }));
+                }
+            }
 
             Modal.Build(anchor, dialog).Show();
         }
