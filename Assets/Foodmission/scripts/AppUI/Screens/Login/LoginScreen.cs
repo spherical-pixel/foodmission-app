42f6168fd9c3b045d4637ab7d9f15410b81519e4
Antonio Jose Duran Sanchez <tonidurans@gmail.com>
2026-04-22 12:14:57 +0200

    - Changed reset password url

--- Assets/Foodmission/scripts/AppUI/Screens/Login/LoginScreen.cs
+++ Assets/Foodmission/scripts/AppUI/Screens/Login/LoginScreen.cs
@@ -124,7 +124,7 @@ namespace eu.foodmission.platform
         private void OnForgotClicked()
         {
             // TODO: We'll need to change this once the endpoint is working
-            Application.OpenURL("https://test.auth.foodmission.eu/realms/foodmission/login-actions/reset-credentials");
+            Application.OpenURL("https://staging.auth.foodmission.eu/realms/foodmission/login-actions/reset-credentials");
             //_navController.Navigate(Actions.login_to_forgotpassword);
         }
 
