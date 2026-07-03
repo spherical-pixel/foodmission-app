ae6feb319153fe1bad4c2aa9ea840ba5110d29aa
Enrique David <undereyes@gmail.com>
2026-07-03 13:18:56 +0200

    test: add PlatformVersionInfo.GetLocalizedReleaseNotes tests

--- Assets/Foodmission/Tests/Editor/AppVersionCheckResultTests.cs
+++ Assets/Foodmission/Tests/Editor/AppVersionCheckResultTests.cs
@@ -39,4 +39,71 @@ namespace eu.foodmission.platform.Tests
             Assert.IsTrue(result.android.isForced);
         }
     }
+
+    [TestFixture]
+    public class PlatformVersionInfoTests
+    {
+        private PlatformVersionInfo CreateInfoWithNotes(string englishNotes = "English notes")
+        {
+            return new PlatformVersionInfo
+            {
+                latestVersion = "1.0.0",
+                releaseNotes = englishNotes,
+                releaseNotes_nl = "Dutch notes",
+                releaseNotes_de = "German notes",
+                releaseNotes_el = "Greek notes",
+                releaseNotes_it = "Italian notes",
+                releaseNotes_no = "Norwegian notes",
+                releaseNotes_pl = "Polish notes",
+                releaseNotes_sl = "Slovenian notes",
+                releaseNotes_es = "Spanish notes"
+            };
+        }
+
+        [Test]
+        public void GetLocalizedReleaseNotes_DefaultLocale_ReturnsEnglish()
+        {
+            var info = CreateInfoWithNotes();
+            Assert.AreEqual("English notes", info.GetLocalizedReleaseNotes("en"));
+        }
+
+        [Test]
+        public void GetLocalizedReleaseNotes_UnknownLocale_ReturnsEnglish()
+        {
+            var info = CreateInfoWithNotes();
+            Assert.AreEqual("English notes", info.GetLocalizedReleaseNotes("fr"));
+        }
+
+        [Test]
+        public void GetLocalizedReleaseNotes_SpanishLocale_ReturnsSpanish()
+        {
+            var info = CreateInfoWithNotes();
+            Assert.AreEqual("Spanish notes", info.GetLocalizedReleaseNotes("es"));
+        }
+
+        [Test]
+        public void GetLocalizedReleaseNotes_LocalizedFieldNull_FallsBackToEnglish()
+        {
+            var info = new PlatformVersionInfo
+            {
+                releaseNotes = "English notes",
+                releaseNotes_es = null
+            };
+            Assert.AreEqual("English notes", info.GetLocalizedReleaseNotes("es"));
+        }
+
+        [TestCase("nl", "Dutch notes")]
+        [TestCase("de", "German notes")]
+        [TestCase("el", "Greek notes")]
+        [TestCase("it", "Italian notes")]
+        [TestCase("no", "Norwegian notes")]
+        [TestCase("pl", "Polish notes")]
+        [TestCase("sl", "Slovenian notes")]
+        [TestCase("es", "Spanish notes")]
+        public void GetLocalizedReleaseNotes_SupportedLocales_ReturnsCorrectValue(string locale, string expected)
+        {
+            var info = CreateInfoWithNotes();
+            Assert.AreEqual(expected, info.GetLocalizedReleaseNotes(locale));
+        }
+    }
 }
