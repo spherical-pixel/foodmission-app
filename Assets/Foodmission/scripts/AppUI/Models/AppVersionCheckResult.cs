b49f25e006986ec55a999cdef046703229e2b5cb
Enrique David <undereyes@gmail.com>
2026-07-03 13:17:14 +0200

    feat: add localized releaseNotes fields and GetLocalizedReleaseNotes method to PlatformVersionInfo

--- Assets/Foodmission/scripts/AppUI/Models/AppVersionCheckResult.cs
+++ Assets/Foodmission/scripts/AppUI/Models/AppVersionCheckResult.cs
@@ -26,5 +26,30 @@ namespace eu.foodmission.platform
         public bool isForced;
         public string storeUrl;
         public string releaseNotes;
+
+        public string releaseNotes_nl;
+        public string releaseNotes_de;
+        public string releaseNotes_el;
+        public string releaseNotes_it;
+        public string releaseNotes_no;
+        public string releaseNotes_pl;
+        public string releaseNotes_sl;
+        public string releaseNotes_es;
+
+        public string GetLocalizedReleaseNotes(string localeCode)
+        {
+            return localeCode switch
+            {
+                "nl" => releaseNotes_nl ?? releaseNotes,
+                "de" => releaseNotes_de ?? releaseNotes,
+                "el" => releaseNotes_el ?? releaseNotes,
+                "it" => releaseNotes_it ?? releaseNotes,
+                "no" => releaseNotes_no ?? releaseNotes,
+                "pl" => releaseNotes_pl ?? releaseNotes,
+                "sl" => releaseNotes_sl ?? releaseNotes,
+                "es" => releaseNotes_es ?? releaseNotes,
+                _ => releaseNotes
+            };
+        }
     }
 }
